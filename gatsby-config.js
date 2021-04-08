2b9ec96e3a960bfa7c0a6ca3551ae81734d8b769
Sthefano Carvalho <sthefano@masteradin.com>
2021-04-08 15:34:33 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  siteMetadata: {
+    title: "Cura Salute",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        url: "http://wordpress.curasalute.com.br/graphql",
+      },
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-3D12SY1GXC",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
