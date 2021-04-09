aa69083ba6c9bd1c19cd60a5ef1095eeef1bb16f
Sthefano Carvalho <sthefano@masteradin.com>
2021-04-08 23:56:48 -0300

    fix: correct siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,40 +1,42 @@
 module.exports = {
   siteMetadata: {
-    title: "Cura Salute",
+    title: 'Cura Salute',
+    description: 'modelo inicial cura salute',
+    siteUrl: 'https://curasalute.com.br',
   },
   plugins: [
     {
-      resolve: "gatsby-source-wordpress",
+      resolve: 'gatsby-source-wordpress',
       options: {
-        url: "http://wordpress.curasalute.com.br/graphql",
+        url: 'http://wordpress.curasalute.com.br/graphql',
       },
     },
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-image",
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-image',
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: "G-3D12SY1GXC",
+        trackingId: 'G-3D12SY1GXC',
       },
     },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
   ],
 };
